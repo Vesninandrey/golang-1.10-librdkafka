c059045aee5bb71f35c137a5a02477402468ba54
Dtim1985 <newdeadsoul@gmail.com>
2018-05-07 15:24:58 +0300

    Try to fix pkg-config issue

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ RUNTIME_PACKAGES="pkg-config openssl git"
 
 export DEBIAN_FRONTEND=noninteractive
 export PATH="${PATH}:/usr/local/go/bin"
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 
 apt-get -y update
 apt-get -y upgrade
@@ -15,7 +16,7 @@ apt-get -y install ${BUILD_PACKAGES} ${RUNTIME_PACKAGES}
 # librdkafka
 git clone --depth 1 -b "${LIBRDKAFKA_REF}" -- https://github.com/edenhill/librdkafka librdkafka
 cd librdkafka
-./configure
+./configure --prefix=/usr
 make
 make install
 ldconfig
